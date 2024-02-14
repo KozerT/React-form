e9d811729e08b0160d8cc0d2924a47d58d58e53b
KozerT <kozer.vizastaff@gmail.com>
2024-02-14 12:57:38 +0100

    feat: Integrated HTML5 form validation to the Signup component,  by using browser build in attributes. Referenced MDN documentation: https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 
-// import Signup from "./components/Signup";
+import Signup from "./components/Signup";
 import Login from "./components/RefLogin";
 // import Login from "./components/StateLogin";
 
@@ -9,7 +9,7 @@ function App() {
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
